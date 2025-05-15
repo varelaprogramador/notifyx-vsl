1e6f337de458e0f69acce130fbcf958410f19a06
Ryan varela <108085059+varelaprogramador@users.noreply.github.com>
2025-05-15 18:25:39 -0300

    Adiciona rastreamento de visualizações de página com Facebook Pixel no layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Sora } from 'next/font/google'
 
 import { Toaster } from '@/components/ui/sonner'
 import { QueryProvider } from '@/providers/query-provider'
-
+import Image from 'next/image'
 export const metadata: Metadata = {
   title: 'Captação | NotifyX',
   description: 'Pagina de captação de leads',
@@ -33,19 +33,31 @@ export default function RootLayout({
     <html lang="pt-BR" suppressHydrationWarning className={sora.className}>
       {process.env.FACEBOOK_PIXEL_ID &&
         process.env.NODE_ENV === 'production' && (
-          <Script id="fb-pixel" strategy="afterInteractive">
-            {`
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '${process.env.FACEBOOK_PIXEL_ID}');
-            `}
-          </Script>
+          <>
+            <Script id="fb-pixel" strategy="afterInteractive">
+              {`
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '${process.env.FACEBOOK_PIXEL_ID}');
+                fbq('track', 'PageView');
+              `}
+            </Script>
+            <noscript>
+              <Image
+                height="1"
+                width="1"
+                style={{ display: 'none' }}
+                alt=""
+                src={`https://www.facebook.com/tr?id=${process.env.FACEBOOK_PIXEL_ID}&ev=PageView&noscript=1`}
+              />
+            </noscript>
+          </>
         )}
 
       <body className="flex size-full min-h-dvh flex-col overflow-x-hidden antialiased">
